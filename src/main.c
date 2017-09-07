598d1e7a6496408b24aaea12e5e547a6bded2118
Bogdan Sinitsyn <bogdan.sinitsyn@gmail.com>
2017-09-07 22:06:38 +0300

    Output messages when playback should be changed

--- src/main.c
+++ src/main.c
@@ -5,10 +5,32 @@
 #include <inttypes.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 
 #define UNUSED __attribute__((unused))
 
+enum port_type {
+  PORT_TYPE_UNKNOWN,
+  PORT_TYPE_SPEAKER,
+  PORT_TYPE_HEADPHONES,
+};
+
+enum port_type last_port_type = PORT_TYPE_UNKNOWN;
+
+enum port_type
+get_port_type(pa_sink_port_info *port) {
+  if (!port) {
+    return PORT_TYPE_UNKNOWN;
+  } else if (strstr(port->name, "speaker")) {
+    return PORT_TYPE_SPEAKER;
+  } else if (strstr(port->name, "headphones")) {
+    return PORT_TYPE_HEADPHONES;
+  } else {
+    return PORT_TYPE_UNKNOWN;
+  }
+}
 
 void
 sink_changed_callback(pa_context * UNUSED ctx,
@@ -17,13 +39,17 @@ sink_changed_callback(pa_context * UNUSED ctx,
                       void * UNUSED userdata)
 {
   if (eol) return;
-
-  pa_sink_port_info *port = info->active_port;
-  if (!port) {
-    perror("port is NULL\n");
+  enum port_type cur_port_type = get_port_type(info->active_port);
+  if (cur_port_type == last_port_type) {
     return;
+  } else if (cur_port_type == PORT_TYPE_SPEAKER) {
+    // pause
+    printf("%s\n", "should pause");
+  } else if (cur_port_type == PORT_TYPE_HEADPHONES) {
+    // play
+    printf("%s\n", "should play");
   }
-  printf("new port name is '%s'\n", port->name);
+  last_port_type = cur_port_type;
 }
 
 void
@@ -33,8 +59,6 @@ subscribe_callback(pa_context *ctx,
                    void * UNUSED userdata)
 {
   if ((type & PA_SUBSCRIPTION_EVENT_TYPE_MASK) != PA_SUBSCRIPTION_EVENT_CHANGE) return;
-
-  printf("Sink %" PRIu32 " changed\n", idx);
   pa_context_get_sink_info_by_index(ctx, idx, sink_changed_callback, NULL);
 }
 
