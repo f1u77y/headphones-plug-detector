e25ddd63d640a66901fea9394990af2686b6231f
Bogdan Sinitsyn <bogdan.sinitsyn@gmail.com>
2017-11-29 21:00:31 +0300

    Use format string in g_warning

--- src/mpris.c
+++ src/mpris.c
@@ -124,7 +124,7 @@ with_player_do(const gchar* name, WithPlayerCallback callback)
   GError* error = NULL;
   MediaPlayer2Player* player = get_player_by_name(name, &error);
   if (!player) {
-    g_warning(error->message);
+    g_warning("%s", error->message);
     g_error_free(error);
     return;
   }
